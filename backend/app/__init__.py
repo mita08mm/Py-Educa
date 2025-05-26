c6555a8ac9f615ebd45249868acc910a0af8b2ee
Mariana Vallejos <marivalleandrea@gmail.com>
2025-05-26 18:44:40 -0400

    Fix: backend test github action

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
-from .config import Config
+from .config import Config, TestingConfig
 from .extensions import db, migrate, jwt
 from .routes import register_routes
 from app.models.modulo import Modulo
 from app.models.seccion import Seccion
 from flask_cors import CORS
 
-def create_app():
+def create_app(config_name="default"):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    config_by_name = {
+        "default": Config,
+        "testing": TestingConfig,
+    }
+    
+    app.config.from_object(config_by_name[config_name])
 
     # Habilitar CORS para permitir peticiones desde el frontend
     CORS(app)
