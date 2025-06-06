cba9eee17ad3d96fe09c0ff521942a89ef365032
Mariana Vallejos <marivalleandrea@gmail.com>
2025-06-06 14:00:28 -0400

    Add tests for creating an evaluation

--- backend/app/models/ejemplo.py
+++ backend/app/models/ejemplo.py
@@ -3,9 +3,11 @@ from app.extensions import db
 class Ejemplo(db.Model):
     __tablename__ = "ejemplos"
 
-    cod_problema = db.Column(db.Integer, db.ForeignKey('problema.cod_problema'), primary_key=True)
     cod_ejemplo = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    cod_problema = db.Column(db.Integer, db.ForeignKey('problema.cod_problema'), nullable=False)
+
     input_ejemplo = db.Column(db.Text)
     output_ejemplo = db.Column(db.Text)
 
     problema = db.relationship("Problema", back_populates="ejemplos")
+
