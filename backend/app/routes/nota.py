23e63836fdb3b566dde580e19becf93bb87d3fa0
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2025-06-09 23:42:53 -0400

    docs: comment nota and seccion

--- backend/app/routes/nota.py
+++ backend/app/routes/nota.py
@@ -1,7 +1,14 @@
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields
 from app.controllers import notaController
 
 api = Namespace('nota', description='Operaciones relacionadas con notas')
+
+nota_model = api.model('Seccion', {
+    'cod_problema': fields.Integer(required=True, description='Código del problema al que pertenece'),
+    'cod_usuario': fields.Integer(required=True, description='Código del usuario al que pertenece la nota'),
+    'nota': fields.Integer(required=True, description='Nota del usuario para el problema'),
+    'nota_total': fields.Integer(required=True, description='Nota del problema'),
+})
 
 @api.route('/')
 class NotaResource(Resource):
@@ -17,6 +24,7 @@ class NotaResource(Resource):
 
 @api.route('/calificar')
 class CalificarResource(Resource):
+    @api.expect(nota_model)
     @api.response(200, 'Problema calificado exitosamente')
     @api.response(400, 'Datos de entrada inválidos')
     def post(self):
--- backend/app/routes/seccion.py
+++ backend/app/routes/seccion.py
@@ -1,12 +1,26 @@
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields
 from app.controllers import seccionController
 
 api = Namespace('seccion', description='Operaciones relacionadas con secciones')
 
+seccion_model = api.model('Seccion', {
+    'titulo_seccion': fields.String(required=True, description='Título de la sección'),
+    'descripcion_seccion': fields.String(required=True, description='Descripción de la sección'),
+    'cod_curso': fields.Integer(required=True, description='Código del curso al que pertenece')
+})
+
 @api.route('/')
 class SeccionListResource(Resource):
+    @api.doc('listar_secciones')
+    @api.response(200, 'Lista de secciones obtenida exitosamente')
     def get(self):
+        """Obtiene todas las secciones disponibles"""
         return seccionController.listar_secciones()
 
+    @api.doc('crear_seccion')
+    @api.expect(seccion_model)
+    @api.response(201, 'Sección creada exitosamente')
+    @api.response(400, 'Datos de entrada inválidos')
     def post(self):
+        """Crea una nueva sección"""
         return seccionController.crear_seccion()
