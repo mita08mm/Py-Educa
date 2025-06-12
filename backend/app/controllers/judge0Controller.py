9c7a156709ca2617ccf756c50317e997effcf4b3
Mariana Vallejos <marivalleandrea@gmail.com>
2025-06-12 17:33:00 -0400

    Fix: delete jsonify function

--- backend/app/controllers/judge0Controller.py
+++ backend/app/controllers/judge0Controller.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify
+from flask import request
 from app.services.judge0Service import submit_code_to_judge0
 import logging
 
@@ -6,13 +6,13 @@ def execute_code_controller():
     try:
         data = request.get_json()
         if not data:
-            return jsonify({"error": "No se proporcionaron datos JSON"}), 400
+            return  {"error": "No se proporcionaron datos JSON"}, 400
             
         code = data.get("code", "")
         stdin = data.get("stdin", "")
 
         if not code:
-            return jsonify({"error": "El campo 'code' es obligatorio"}), 400
+            return  {"error": "El campo 'code' es obligatorio"}, 400
 
         logging.info("Executing code through Judge0...")
         result = submit_code_to_judge0(code, stdin)
@@ -34,4 +34,4 @@ def execute_code_controller():
     except Exception as e:
         error_msg = str(e)
         logging.error(f"Error executing code: {error_msg}")
-        return jsonify({"error": error_msg}), 500
+        return  {"error": error_msg}, 500
--- backend/app/controllers/seccionController.py
+++ backend/app/controllers/seccionController.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify
+from flask import request
 from app.services.seccionService import get_all_secciones, create_seccion
 from app.schemas.seccionSchema import SeccionSchema
 
