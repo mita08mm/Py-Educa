1910807d468b5390bca538a7a5b9f7453b711b52
Jhair <jhaircjxd@gmail.com>
2025-06-21 18:27:59 -0400

    fix: jsonify bugs and deleting unnecesary model

--- backend/app/controllers/evaluacionController.py
+++ backend/app/controllers/evaluacionController.py
@@ -1,11 +1,11 @@
-from flask import request, jsonify
+from flask import request
 from app.services.evaluacionService import crear_evaluacion, obtener_evaluacion_por_modulo
 
 def crear_evaluacion_controller():
     data = request.get_json()
     resultado, status_code = crear_evaluacion(data)
-    return jsonify(resultado), status_code
+    return resultado, status_code
 
 def obtener_evaluacion_por_modulo_controller(cod_modulo):
     resultado, status_code = obtener_evaluacion_por_modulo(cod_modulo)
-    return jsonify(resultado), status_code
+    return resultado, status_code
